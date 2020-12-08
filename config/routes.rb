b37a5ce43f0bf17154df9c8caf977ca59bf82573
masaki511 <74614174+masaki511@users.noreply.github.com>
2020-12-08 23:50:13 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] do
+  end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
